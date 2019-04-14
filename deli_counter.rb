4944ec26e5b9ded7f8c7ffee76f587bad1f4e67b
Kelley Chaplain <kmchaplain@gmail.com>
2019-04-14 18:12:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,53 @@
-# Write your code here.
+=begin
+katz_deli = [ ]
+
+def line(katz_deli)
+  
+  peeps = [ ]
+  number = 0
+  
+  if katz_deli.empty? == true
+    puts "The line is currently empty."
+else
+    katz_deli.each do |name|
+      number += 1
+        peeps << "#{number}. #{name}"
+    puts "The line is currently: #{peeps}"
+  end  
+end  
+=end
+
+def take_a_number(katz_deli, new)
+  num = [1..]
+  katz_deli << new
+  h = { }
+    h[katz_deli.zip(num)]
+  
+  h.each {|katz_deli, num| puts "Welcome, #{katz_deli}. You are number #{num} in line."}    
+    
+  if katz_deli.empty?
+    katz_deli.push(new)
+      puts "Welcome, #{name}. You are number #{num} in line."
+      
+  end
+  
+end  
+
+def now_serving(katz_deli)
+  
+  first = katz_deli.first
+  
+  if katz_deli.empty? == true
+    puts "There is nobody waiting to be served!"
+    
+  else 
+    puts "Currently serving #{first}."
+  
+  end  
+  
+  katz_deli.shift
+  
+end
+
+
+
